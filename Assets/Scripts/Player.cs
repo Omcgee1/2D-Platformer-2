fd77788f15e341a84fc63205827e77784dddedff
Omcgee1 <omcgee1@stu.chi.ac.uk>
2022-02-14 11:36:35 +0000

    Implemented player movement code (not working yet)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    [SerializeField] float playerSpeed = 5f;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float playerInput = playerInput.GetAxis("Horizontal");
+        rb.velocity = new Vector2(playerInput * playerSpeed, rb.velocity.y);
     }
 }
